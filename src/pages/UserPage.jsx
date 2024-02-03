01769db9029a426e143155f9b37cf64f76b2c3b2
chamudithalakshan <chamudithawickramarathna@gmail.com>
2024-02-03 10:59:01 +0530

    get user done

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -14,6 +14,8 @@ export default function UserPage() {
     const [uid, setUid] = useState('')
     const [email, setEmail] = useState('')
     const [token,setToken] = useState('wys_84a7qaHZ0F8pJ1iEsjhWZyJ72DykCO11BgxO')
+
+    const [users, setUsers] = useState([])
     const addUser = () => {
         // Make a POST request to your server
         fetch('https://73ffef27f91c463695b25d6ac6444e68.weavy.io/api/users', {
@@ -30,7 +32,7 @@ export default function UserPage() {
         })
           .then(response => response.json())
           .then(newUser => {
-            console.log(newUser)
+            getUsers()
             // Update the local state with the new user
             // setUsers([...users, newUser]);
           })
@@ -38,6 +40,27 @@ export default function UserPage() {
             console.error('Error adding user:', error);
           });
       };
+
+      const getUsers= ()=>{
+        fetch('https://73ffef27f91c463695b25d6ac6444e68.weavy.io/api/users', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        })
+          .then(response => response.json())
+          .then(users => {
+            // console.log(users?.data[0].name)
+            setUsers(users.data);
+          })
+          .catch(error => {
+            console.error('Error adding user:', error);
+          });
+      }
+
+      useEffect(()=>{
+        getUsers();
+      },[])
     
   return (
     <div>
@@ -53,15 +76,13 @@ export default function UserPage() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">uid</TableCell>
+            <TableCell align="right">Email</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {/* {rows.map((row) => (
+          {users?.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -69,12 +90,10 @@ export default function UserPage() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.uid}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
             </TableRow>
-          ))} */}
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
